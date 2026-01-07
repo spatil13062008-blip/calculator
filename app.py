090f0f415dc18856d18e6649452bd4d56014dde0
spatil13062008-blip <spatil13062008@gmail.com>
2026-01-07 16:43:43 +0530

    Initial upload

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/calculate", methods=["POST"])
+def calculate():
+    num1 = float(request.form["num1"])
+    num2 = float(request.form["num2"])
+    operator = request.form["operator"]
+
+    if operator == "+":
+        result = num1 + num2
+    elif operator == "-":
+        result = num1 - num2
+    elif operator == "*":
+        result = num1 * num2
+    elif operator == "/":
+        result = num1 / num2 if num2 != 0 else "Division by zero not allowed"
+    else:
+        result = "Invalid operator"
+
+    return render_template("index.html", result=result)
+
+if __name__ == "__main__":
+    app.run(debug=True)
